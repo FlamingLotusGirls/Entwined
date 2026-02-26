55abdc1fd35ad8f90fe93845a344eb64a9b4c2b5
kmurphy <murphykatherine102@gmail.com>
2026-02-25 23:02:15 -0800

    Sunset pattern: Add a discrete spin to the cockatoo windows for a blender or something

--- chromatik/src/main/java/entwined/pattern/katie_murphy/Sunset.java
+++ chromatik/src/main/java/entwined/pattern/katie_murphy/Sunset.java
@@ -3,9 +3,11 @@ package entwined.pattern.katie_murphy;
 import heronarts.lx.color.LXColor;
 import heronarts.lx.LX;
 import heronarts.lx.model.LXPoint;
+import heronarts.lx.modulator.SawLFO;
 import heronarts.lx.modulator.SinLFO;
-import heronarts.lx.parameter.CompoundParameter;
 import heronarts.lx.parameter.BooleanParameter;
+import heronarts.lx.parameter.CompoundParameter;
+import heronarts.lx.parameter.DiscreteParameter;
 import heronarts.lx.pattern.LXPattern;
 import entwined.utils.SimplexNoise;
 import heronarts.lx.model.LXModel;
@@ -22,13 +24,13 @@ public class Sunset extends LXPattern {
   final SinLFO greenWave = new SinLFO(140, 155, 2000);
   final SinLFO yellowWave = new SinLFO(40, 50, 2000);
   final SinLFO redWave = new SinLFO(0, 20, 2000);
+  final SawLFO sawWave = new SawLFO(0, 360, 1000);
 
   private float minz = Float.MAX_VALUE;
   private float maxz = -Float.MAX_VALUE;
-  private float speedMult = 1000;
-  private int cheekPressCounter = 0;
 
   float height = 0.0f;
+  float sawHeight = 0.0f;
 
   final CompoundParameter fillParam = new CompoundParameter("BRT", 25f, 0.001f,
         100f);
@@ -43,6 +45,9 @@ public class Sunset extends LXPattern {
   private double cockatooCheekFlashEnd = 0;
   final BooleanParameter cockatooCheekParam = new BooleanParameter("cCheek", false).setMode(BooleanParameter.Mode.MOMENTARY);
 
+  // 0 = 0ff, 1-3 = low/med/high
+  public final DiscreteParameter cockatooWindowSpin = new DiscreteParameter("cWinSpin", 0, 4);
+  
 
   public Sunset(LX lx) {
     super(lx);
@@ -61,15 +66,13 @@ public class Sunset extends LXPattern {
       }
     });
 
+    addParameter(cockatooWindowSpin);
+
+    addModulator(sawWave).start();
     addModulator(blueWave).start();
     addModulator(greenWave).start();
     addModulator(yellowWave).start();
     addModulator(redWave).start();
-  }
-
-  private void startCheekFlash() {
-    cockatooCheekFlashActive = true;
-    cockatooCheekFlashEnd = EntwinedUtils.millis() + cheekFlashDurationMs;
   }
 
   private void renderDefault(double deltaMs) {
@@ -136,15 +139,15 @@ public class Sunset extends LXPattern {
   }
 
   private void applyTagOverrides(double deltaMs) {
+    sawHeight = sawWave.getValuef();
+
     for (LXModel component : model.children) {
       for (LXPoint point : component.points) {
 
         if (component.tags.contains("Cheek")) {
-          if (cockatooCheekFlashActive) {
-            double remainingMs = cockatooCheekFlashEnd - EntwinedUtils.millis();
-            float t = (float)(remainingMs / cheekFlashDurationMs);
-            colors[point.index] = LXColor.lerp(colors[point.index], LXColor.RED, t);
-          }
+          applyCockatooCheekOverrides(deltaMs, point);
+        } else if (component.tags.contains("CockatooSegment") && component.tags.contains("WindowPane")) {
+          applyCockatooWindowOverrides(deltaMs, point);
         }
 
       }
@@ -165,6 +168,27 @@ public class Sunset extends LXPattern {
 
     // do trigger overrides 
     applyTagOverrides(deltaMs); 
+  }
+
+  /////////////// Cockatoo Cheek Functions
+  private void applyCockatooCheekOverrides(double deltaMs, LXPoint point) {
+    if (cockatooCheekFlashActive) {
+      double remainingMs = cockatooCheekFlashEnd - EntwinedUtils.millis();
+      float t = (float)(remainingMs / cheekFlashDurationMs);
+      colors[point.index] = LXColor.lerp(colors[point.index], LXColor.RED, t);
+    }
+  }
+
+  private void startCheekFlash() {
+    cockatooCheekFlashActive = true;
+    cockatooCheekFlashEnd = EntwinedUtils.millis() + cheekFlashDurationMs;
+  }
+
+  private void applyCockatooWindowOverrides(double deltaMs, LXPoint point) {
+
+    if (cockatooWindowSpin.getValue() > 0) {
+    colors[point.index] = HavenDefaultInputsUtils.SpinningStainedRun(lx, point, sawHeight * cockatooWindowSpin.getValuef());
+    }
   }
 
 }
